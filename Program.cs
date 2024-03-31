978b8d6abaedc72c7ffdfb295271696f4cd32311
shudera <viniciusyashuda008@gmail.com>
2024-03-31 18:44:44 -0300

    Implementando lógica interna do sistema

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia("12345", "Tijolao", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone("5697", "IPhone 13", "222222", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
